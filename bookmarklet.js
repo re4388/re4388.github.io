41287cae9410337c7e3bf57bc5a473d1f693cbf3
re4388 <re4388@gmail.com>
2018-10-20 23:01:39 +0800

    Create bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,9 @@
+(function () {
+  console.log("It started");
+  let para = document.getElementsByTagName("p");
+
+  for (let i = 0; i < para.length; i++) {
+    const element = para[i];
+    element.innerHTML="Sabrina" 
+  }
+})();
